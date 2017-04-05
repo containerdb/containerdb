7701d975b011d347766da1012c53c3cef7345a8c
Scott Robertson <scottymeuk@gmail.com>
2017-04-06 00:17:54 +0100

    Comments

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -9,8 +9,11 @@ class BaseService
     raise NotImplementedError.new('This method has not been implemented for this Service')
   end
 
+  # Environment Variables
   alias_method :default_environment_variables,    :not_implemented!
   alias_method :required_environment_variables,   :not_implemented!
+
+  # Container Settings
   alias_method :connection_string,                :not_implemented!
   alias_method :connection_command,               :not_implemented!
   alias_method :container_port,                   :not_implemented!
