99aeeb2fcf11e68b03e71f76f4bcc81a32bfa89d
Scott Robertson <scottymeuk@gmail.com>
2017-04-16 00:24:04 +0100

    Some nginx changes

--- install.sh
+++ install.sh
@@ -71,6 +71,10 @@ if ! $installed; then
 server {
   listen          80;
   location / {
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header Host $http_host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_redirect off;
     proxy_pass      http://localhost:6000;
   }
 }
