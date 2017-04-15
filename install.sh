f9f3bd5d7f32a2511131ee74b55e5ee3fab5f997
Scott Robertson <scottymeuk@gmail.com>
2017-04-16 00:50:25 +0100

    We have to escape the variables... duh

--- install.sh
+++ install.sh
@@ -73,9 +73,9 @@ server {
   listen 80;
   location / {
     proxy_pass http://localhost:6000;
-    proxy_set_header X-Forwarded-Proto $scheme;
-    proxy_set_header Host $http_host;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header Host \$http_host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_redirect off;
   }
 }
