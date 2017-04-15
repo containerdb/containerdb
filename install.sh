7a24de8dc81a85ab74605f81033fcd110f9b6d2d
Scott Robertson <scottymeuk@gmail.com>
2017-04-15 21:43:01 +0100

    Bit more output

--- install.sh
+++ install.sh
@@ -1,25 +1,33 @@
 echo 'Starting Container DB installer'
 echo ''
 
+echo 'Please enter your AWS credentials for backups'
 read -p 'AWS_SECRET_KEY: ' AWS_SECRET_KEY
 read -p 'AWS_ACCESS_TOKEN: ' AWS_ACCESS_TOKEN
 read -p 'AWS_BUCKET_NAME: ' AWS_BUCKET_NAME
+echo 'Thanks...'
+echo ''
 
 # @todo test these keys work
 
+echo 'Installing required packages'
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y pwgen
+echo ''
 
 # Pull the base images
+echo 'Pulling required Docker images'
 docker pull containerdb/backup-restore
 docker pull containerdb/containerdb
 docker pull postgres
 docker pull mysql
 docker pull tutum/redis
+echo ''
 
+echo 'Installing Container DB'
 # Create the Postgres Container
 DB_CONTAINER_NAME='containerdb_db'
 DB_PORT=8474
@@ -43,3 +51,6 @@ docker exec containerdb_app rails db:create db:migrate
 
 # Let Container DB manage it's own DB
 docker exec containerdb_app rails r "Service.create!(service_type: :postgres, name: 'containerdb', port: $DB_PORT, container_id: '$DB_CONTAINER_ID', environment_variables: { 'POSTGRES_PASSWORD' => '$DB_PASSWORD', 'POSTGRES_USER' => '$DB_USERNAME'})"
+echo ''
+echo '...done'
+echo 'Visit http://$HOST_IP'
