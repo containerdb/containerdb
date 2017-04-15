4be522235d6b61e29a6537197972eb2a68b2225d
Scott Robertson <scottymeuk@gmail.com>
2017-04-15 19:12:57 +0100

    Postgres backups

--- app/services/postgres_service.rb
+++ app/services/postgres_service.rb
@@ -21,4 +21,21 @@ class PostgresService < BaseService
   def container_port
     5432
   end
+
+  def backup_environment_variables
+    {
+      DB_USER: service.environment_variables['POSTGRES_USER'],
+      DB_HOST: ENV['HOST'],
+      DB_PORT: service.port,
+      DB_PASS: service.environment_variables['POSTGRES_PASSWORD'],
+    }
+  end
+
+  def backup_file_suffix
+    'sql'
+  end
+
+  def backup_script_path
+    'app/postgres/backup.sh'
+  end
 end
