b433cc80ff2060945d9800feb5aea57ca8fbce0d
Marc Qualie <marc@marcqualie.com>
2017-04-18 09:59:43 +0100

    Use sidekiq adapter in development to catch errors with jobs Awaiting pull request merge to core for AsyncAdapter: https://github.com/rails/rails/pull/28582/files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.active_job.queue_adapter = :sidekiq
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
