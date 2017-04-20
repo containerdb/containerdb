50d31400ecd65a0c1be02efa2229c7c42384b083
Scott Robertson <scottymeuk@gmail.com>
2017-04-20 01:17:37 +0100

    Adding a wait, and some whitespace to make it more readable.

--- app/jobs/start_service_job.rb
+++ app/jobs/start_service_job.rb
@@ -1,13 +1,16 @@
 class StartServiceJob < ApplicationJob
   def perform(service)
     return if service.container_id
+
     Rails.logger.info("Pulling Image #{service.image} for Service ##{service.id}")
     image = Docker::Image.create(
       'fromImage' => service.image,
     )
+
     Rails.logger.info("Pulled Image #{service.image} for Service ##{service.id}")
     Rails.logger.debug(image)
     Rails.logger.info("Creating Container for Service ##{service.id}")
+
     container = Docker::Container.create(
       'name' => "#{service.image.parameterize}-#{service.id}",
       'Image' => service.image,
@@ -25,9 +28,14 @@ class StartServiceJob < ApplicationJob
         },
       },
     )
+
     Rails.logger.info("Starting Container #{container.id} for Service ##{service.id}")
     container.start
+
+    Rails.logger.info("Waiting for Container #{container.id} for Service ##{service.id}")
+    container.wait
     Rails.logger.info("Started Container #{container.id} for Service ##{service.id}")
+
     service.update!(container_id: container.id)
     Rails.logger.debug(container)
   end
