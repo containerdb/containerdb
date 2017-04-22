c620348d1b5e4b0605d1b36bbcab0f4198e36f60
Scott Robertson <scottymeuk@gmail.com>
2017-04-22 12:03:56 +0100

    Use env vars for mysql username

--- app/services/mysql_service.rb
+++ app/services/mysql_service.rb
@@ -1,20 +1,21 @@
 class MysqlService < BaseService
   def default_environment_variables
     {
-      'MYSQL_ROOT_PASSWORD' => SecureRandom.hex
+      'MYSQL_ROOT_PASSWORD' => SecureRandom.hex,
+      'MYSQL_ROOT_USERNAME' => 'root'
     }
   end
 
   def required_environment_variables
-    ['MYSQL_ROOT_PASSWORD']
+    ['MYSQL_ROOT_PASSWORD', 'MYSQL_ROOT_USERNAME']
   end
 
   def connection_string
-    "mysql://root:#{service.environment_variables['MYSQL_ROOT_PASSWORD']}@#{service.host}:#{service.port}"
+    "mysql://#{service.environment_variables['MYSQL_ROOT_USERNAME']}:#{service.environment_variables['MYSQL_ROOT_PASSWORD']}@#{service.host}:#{service.port}"
   end
 
   def connection_command
-    "mysql -h#{service.host} -uroot -p#{service.environment_variables['MYSQL_ROOT_PASSWORD']} -P#{service.port}"
+    "mysql -h#{service.host} -u#{service.environment_variables['MYSQL_ROOT_USERNAME']} -p#{service.environment_variables['MYSQL_ROOT_PASSWORD']} -P#{service.port}"
   end
 
   def container_port
@@ -23,7 +24,7 @@ class MysqlService < BaseService
 
   def backup_environment_variables
     {
-      DB_USER: 'root',
+      DB_USER: service.environment_variables['MYSQL_ROOT_USERNAME'],
       DB_HOST: service.host,
       DB_PORT: service.port,
       DB_PASS: service.environment_variables['MYSQL_ROOT_PASSWORD'],
