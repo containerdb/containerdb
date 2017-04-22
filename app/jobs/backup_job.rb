6eabed53b8ad982aea353ce1e122522dbcd8dd4d
Scott Robertson <scottymeuk@gmail.com>
2017-04-22 19:30:34 +0100

    Start using keys from backup provider

--- app/jobs/backup_job.rb
+++ app/jobs/backup_job.rb
@@ -1,12 +1,18 @@
 class BackupJob < ApplicationJob
 
   def perform(backup)
+    Rails.logger.info("Starting backup for #{backup.service.id}")
+    unless backup.service.backup_storage_provider.present?
+      backup.failed!
+      return false
+    end
+
     backup.running!
     file_name = backup.service.backup_file_name
     environment_variables = backup.service.backup_environment_variables.merge({
-      AWS_ACCESS_TOKEN: ENV['AWS_ACCESS_TOKEN'],
-      AWS_SECRET_KEY: ENV['AWS_SECRET_KEY'],
-      AWS_BUCKET_NAME: ENV['AWS_BUCKET_NAME'],
+      AWS_ACCESS_TOKEN: backup.backup_storage_provider.environment_variables['AWS_ACCESS_TOKEN'],
+      AWS_SECRET_KEY: backup.backup_storage_provider.environment_variables['AWS_SECRET_KEY'],
+      AWS_BUCKET_NAME: backup.backup_storage_provider.environment_variables['AWS_BUCKET_NAME'],
       FILE_NAME: file_name
     })
 
@@ -14,15 +20,17 @@ class BackupJob < ApplicationJob
     container = image.run("sh #{backup.service.backup_script_path}", { 'Env' => environment_variables.map {|key, value| "#{key}=#{value}" } })
 
     response = container.wait(3600)
+    Rails.logger.info(container.logs(stderr: true))
+    Rails.logger.info(container.logs(stdout: true))
+
     if response['StatusCode'] == 0
       backup.update(file_name: file_name)
       backup.complete!
     else
-      puts container.logs(stderr: true)
-      puts container.logs(stdout: true)
       backup.failed!
     end
   rescue
+    Rails.logger.info("failed")
     backup.failed!
     raise
   end
--- app/services/storage_provider/s3_provider.rb
+++ app/services/storage_provider/s3_provider.rb
@@ -1,4 +1,10 @@
 class StorageProvider::S3Provider < StorageProvider::BaseProvider
 
+  def default_environment_variables
+    []
+  end
 
+  def required_environment_variables
+    ['AWS_ACCESS_TOKEN', 'AWS_SECRET_KEY', 'AWS_BUCKET_NAME']
+  end
 end
