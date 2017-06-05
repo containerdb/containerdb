ea0ca125498411cc6da608ab1d6d1434871ffbf7
Scott Robertson <scottymeuk@gmail.com>
2017-06-05 16:15:04 +0100

    Redirect to root

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,7 +7,7 @@ class ServicesController < ApplicationController
   def destroy
     @service = Service.find(params[:id])
     @service.destroy unless @service.locked?
-    redirect_to :back
+    redirect_to root_path
   end
 
   def new
