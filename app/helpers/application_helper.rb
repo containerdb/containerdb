8357727c7a17046b1a1030d99615a5f300efe3d3
Scott Robertson <scottymeuk@gmail.com>
2017-06-05 20:45:54 +0100

    Move this to the bottom

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
 
   def docker_version
     Docker.ping
-    "#{Docker.url} v#{Docker.version['Version']}"
+    content_tag :span, "#{Docker.url} v#{Docker.version['Version']}", class: 'text-success'
+
   rescue # Probably should not catch all errors
     content_tag :span, 'Disconnected', class: 'text-danger'
   end
