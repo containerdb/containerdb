4e3d01690a67dc2a1972b8b96f4c8db0a6a3f3bd
Scott Robertson <scottymeuk@gmail.com>
2017-06-16 11:13:08 +0100

    Rails 5 requires redirect_back

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -2,12 +2,12 @@ class BackupsController < ApplicationController
   def create
     service = Service.find(params[:service_id])
     service.backup
-    redirect_to :back
+    redirect_back fallback_location: service
   end
 
   def destroy
     service = Service.find(params[:service_id])
     service.backups.find(params[:id]).destroy
-    redirect_to :back
+    redirect_back fallback_location: service
   end
 end
